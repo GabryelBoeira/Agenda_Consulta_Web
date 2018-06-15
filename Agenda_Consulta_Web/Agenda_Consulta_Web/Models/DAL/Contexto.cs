77608abb7f6883f137848ca12c6b3778cbe664e7
Gabryel <gabryel@gmail.com>
2018-06-14 21:06:28 -0300

    Contexto para a criação do banco de dados concluida

---
+++ Agenda_Consulta_Web/Agenda_Consulta_Web/Models/DAL/Contexto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Agenda_Consulta_Web.Models.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base("strConn") {
+
+            //(Apaga toda a base cada vez que executa)
+            //Database.SetInitializer<Contexto>(new DropCreateDatabaseAlways<Contexto>);
+
+            //Migrantions (Utilizar para produção)
+            Database.SetInitializer<Contexto>(new DropCreateDatabaseIfModelChanges<Contexto>());
+
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Local> Locais { get; set; }
+        public DbSet<Profissional> Profissionais { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+    }
+}
